5b20c42cd34b4c04d6042769c1b958ec9a832fde
avsh <avterus@gmail.com>
2016-04-17 15:33:20 +0300

    add translations

---
+++ begatewayerip/translations/ru.php
@@ -0,0 +1,15 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{begatewayerip}prestashop>begatewayerip_4ef89c80592d148b28870c6a473f0feb'] = 'Система Расчёт (ЕРИП)';
+$_MODULE['<{begatewayerip}prestashop>begatewayerip_e6e9222d2b99d8aea25d073a2e7e878f'] = 'Принимайте платежи через ЕРИП с bePaid.by';
+$_MODULE['<{begatewayerip}prestashop>begatewayerip_48db8327ef2854af02e6a1c57e1acbea'] = 'cURL PHP расширение должно быть включено на сервере, чтобы использовать этот модуль.';
+$_MODULE['<{begatewayerip}prestashop>begatewayerip_22d8ff492a13fa906407341fb129ccfc'] = 'Настройки сохранены';
+$_MODULE['<{begatewayerip}prestashop>configuration_59c03620e217d5059ed1bcef985b0f67'] = 'Задайте настройки, чтобы принимать платежи через систему Расчет (ЕРИП)';
+$_MODULE['<{begatewayerip}prestashop>configuration_24f6d0a5e981497218465d35e9702dae'] = 'ID магазина';
+$_MODULE['<{begatewayerip}prestashop>configuration_5bb912794738b581d373fb67a44c90b7'] = 'Ключ магазина';
+$_MODULE['<{begatewayerip}prestashop>configuration_d125d168a6899e57371eaac4505672b2'] = 'Домен API';
+$_MODULE['<{begatewayerip}prestashop>configuration_318cb3d63237f08b18226fa35b598edd'] = 'Код услуги ЕРИП';
+$_MODULE['<{begatewayerip}prestashop>configuration_f2e3f707d57e6441543b167c0e908dcd'] = 'Сохранить настройки';
+$_MODULE['<{begatewayerip}prestashop>begatewayerip_26848989c34eb11b252746f847a87356'] = 'Оплатить через Расчёт (ЕРИП)';
